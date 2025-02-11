adba9a0ce7849692b6726f3af24b45ca1b452129
handev-id <mparhanmaulana17@gmail.com>
2025-02-11 20:11:16 +0700

    update techstack

---
+++ src/common/techstack.ts
@@ -0,0 +1,63 @@
+import express from "../assets/techstack/express.png";
+import react from "../assets/techstack/react-js.png";
+import typescript from "../assets/techstack/typescript.png";
+import node from "../assets/techstack/node-js.png";
+import ubuntu from "../assets/techstack/ubuntu.png";
+import nest from "../assets/techstack/nest-js.png";
+import bootstrap from "../assets/techstack/bootstrap.png";
+import tailwind from "../assets/techstack/tailwind-css.webp";
+import git from "../assets/techstack/git.png";
+import laravel from "../assets/techstack/laravel.png";
+import adonis from "../assets/techstack/adonis.png";
+import js from "../assets/techstack/js.png";
+
+export const techStacks = [
+  {
+    name: "ReactJS",
+    image: react,
+  },
+  {
+    name: "NodeJS",
+    image: node,
+  },
+  {
+    name: "ExpressJS",
+    image: express,
+  },
+  {
+    name: "NestJS",
+    image: nest,
+  },
+  {
+    name: "Typescript",
+    image: typescript,
+  },
+  {
+    name: "Ubuntu",
+    image: ubuntu,
+  },
+  {
+    name: "Git",
+    image: git,
+  },
+  {
+    name: "Laravel",
+    image: laravel,
+  },
+  {
+    name: "AdonisJS",
+    image: adonis,
+  },
+  {
+    name: "Bootstrap",
+    image: bootstrap,
+  },
+  {
+    name: "TailwindCSS",
+    image: tailwind,
+  },
+  {
+    name: "Javascript",
+    image: js,
+  },
+];
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,5 @@
 import Slider from "react-slick";
+import { techStacks } from "../common/techstack";
 
 const Skills = () => {
   const settings = {
@@ -22,147 +23,30 @@ const Skills = () => {
         <div className="col-12 mb-4">
           <h2>Skills</h2>
         </div>
-        <div className="col-lg-3" style={{ fontSize: "20px" }}>
+        <div className="col-lg-6" style={{ fontSize: "20px" }}>
           <i className="fa-solid fa-globe mx-2"></i>
           Frontend Development
         </div>
-        <div className="col-lg-3" style={{ fontSize: "20px" }}>
+        <div className="col-lg-6" style={{ fontSize: "20px" }}>
           <i className="fa-solid fa-globe mx-2"></i>
           Backend Development
-        </div>
-        <div className="col-lg-3" style={{ fontSize: "20px" }}>
-          <i className="fa-solid fa-mobile-screen-button mx-2"></i>
-          Mobile Development
-        </div>
-        <div className="col-lg-3" style={{ fontSize: "20px" }}>
-          <i className="fa-solid fa-server mx-2"></i>
-          Deployment
         </div>
         <div className="col-12 mb-4 mt-5">
           <h3>TechStack</h3>
         </div>
         <div className="slider-container mb-3">
           <Slider {...settings}>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/1.webp"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/2.webp"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/4.webp"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/5.webp"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/6.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/7.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/9.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/10.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/11.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/12.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/15.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/16.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/18.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/20.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
-            <div>
-              <img
-                style={{ width: "70px", height: "70px" }}
-                src="/techstack/21.png"
-                alt=""
-                className="mx-auto"
-              />
-            </div>
+            {techStacks.map((item) => (
+              <div className="flex flex-col items-center">
+                <img
+                  style={{ width: "70px", height: "70px" }}
+                  src={item.image}
+                  alt={item.name}
+                  className="mx-auto"
+                />
+                <p className="mt-2 text-sm text-gray-700">{item.name}</p>
+              </div>
+            ))}
           </Slider>
         </div>
       </div>
