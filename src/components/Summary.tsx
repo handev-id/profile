5e188d5e0ac705ffdc070c6f11b44472873ac805
Muhamad Parhan Maulana <129810163+handev-id@users.noreply.github.com>
2026-02-26 14:31:59 -0800

    Refactor Summary component for responsive width

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,30 +1,31 @@
 import { Viewer, Worker } from "@react-pdf-viewer/core";
 import { Project } from "../apis/models/project";
 import "@react-pdf-viewer/core/lib/styles/index.css";
+import { useEffect, useState } from "react";
 
 const Summary = ({ projects }: { projects: Project[] }) => {
+  const [width, setWidth] = useState(650);
+
+  useEffect(() => {
+    setWidth(window.innerWidth < 600 ? window.innerWidth : 650);
+  }, []);
+
   return (
-    <div
-      style={{
-        width: window.innerWidth < 600 ? "100%" : "650px",
-        margin: "0 auto",
-      }}
-    >
+    <div style={{ width, margin: "0 auto" }}>
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-        <div>
-          <Viewer
-            fileUrl={"/CV_M_PARHAN_ID.pdf"}
-          />
-          <div className="mt-3 d-flex justify-content-center ">
+        <>
+          <Viewer fileUrl="/CV_M_PARHAN_ID.pdf" />
+          <div className="mt-3 d-flex justify-content-center">
             <a
               className="text-white"
-              href={"/CV_M_PARHAN_ID.pdf"}
+              href="/CV_M_PARHAN_ID.pdf"
               target="_blank"
+              rel="noopener noreferrer"
             >
               Download CV
             </a>
           </div>
-        </div>
+        </>
       </Worker>
     </div>
   );
