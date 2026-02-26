3a4795c79106e9c9e060b90801dae7e9dc584f45
Muhamad Parhan Maulana <129810163+handev-id@users.noreply.github.com>
2026-02-26 14:27:22 -0800

    Refactor Summary component to use static CV file
    Removed console log and updated fileUrl to a static path.

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -3,12 +3,6 @@ import { Project } from "../apis/models/project";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 
 const Summary = ({ projects }: { projects: Project[] }) => {
-  console.log(
-    `${import.meta.env.VITE_BACKEND_URL}/storage/projects/`.replace(
-      "/api",
-      ""
-    ) + projects.find((p) => p.title === "cv")?.images[0]
-  );
   return (
     <div
       style={{
@@ -19,13 +13,7 @@ const Summary = ({ projects }: { projects: Project[] }) => {
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
         <div>
           <Viewer
-            fileUrl={
-              `${import.meta.env.VITE_BACKEND_URL}/storage/projects/`.replace(
-                "/api",
-                ""
-              ) + projects.find((p) => p.title === "cv")?.images[0] ||
-              "/HANDEV_CV.pdf"
-            }
+            fileUrl={"/CV_M_PARHAN_ID.pdf"}
           />
           <div className="mt-3 d-flex justify-content-center ">
             <a
